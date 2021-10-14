4fad08b7d54558a68460e1edd733555c17436cff
Tommy <13759796+ern1@users.noreply.github.com>
2021-10-14 10:25:22 +0200

    Added control via serial + Some other changes

--- project/project.ino
+++ project/project.ino
@@ -1,25 +1,22 @@
 #define FASTLED_INTERNAL
 #include <FastLED.h>
 
-// Colors
-#define GREEN 128, 0, 0    // CRGB::Green
-#define YELLOW 255, 165, 0 // CRGB::Orange
-#define RED 0, 255, 0      // CRGB::Red
 
 #define NUM_LEDS 11
 #define LED_PIN 3
+#define MAX_LED_BRIGHTNESS 255
 
 #define PIEZO_PIN A0
-#define PIEZO_THRESHOLD 100
+#define PIEZO_THRESHOLD 130
 
-#define TIME_ON 6000 // ms before leds turn off
+#define TIME_ON 10000 // ms before leds turn off
 
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 const int num = 32;
 int activityHigh = 5, activityMed = 4, activityLow = 2;
-int ledBrightness = 255;
+int ledBrightness = MAX_LED_BRIGHTNESS;
 
 // switch and shut off timer
 int sensorReading = 0;
@@ -27,22 +24,6 @@ int timerOn = TIME_ON;
 unsigned long lastTime = 0;
 
 
-void setLeds(int start, int end, byte r, byte g, byte b) {
-  for (int i = start; i < end; i++) {
-    leds[i] = CRGB (r, g, b);
-  }
-  FastLED.show();
-}
-
-void setAllLeds(int green, int yellow, int red) {
-  int d1 = green, d2 = yellow + green, d3 = red + yellow + green;
-  setLeds(0, d1, GREEN);
-  setLeds(d1, d2, YELLOW);
-  setLeds(d2, d3, RED);
-}
-
-// TODO: testa köra med dessa istället (kan ta bort defines för colors om det funkar)
-/*
 void setLeds(int start, int end, const struct CRGB &color) {
   for (int i = start; i < end; i++) {
     leds[i] = color;
@@ -56,15 +37,36 @@ void setAllLeds(int green, int yellow, int red) {
   setLeds(d1, d2, CRGB::Orange);
   setLeds(d2, d3, CRGB::Red);
 }
-*/
 
-// TODO: testa om det funkar att bara köra FastLED.clear() istället för att sätta alla till 0, 0, 0
 void setLedsOff() {
-  //for (int i = 0; i < NUM_LEDS; i++) {
-  //  leds[i] = CRGB (0, 0, 0);
-  //}
   FastLED.clear();
   FastLED.show();
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    Serial.println(inChar);
+
+    switch(inChar) {
+      case 'q':
+        if (activityHigh == 0) break;
+        activityHigh--; activityMed++;
+        break;
+      case 'w':
+        if (activityMed == 0) break;
+        activityHigh++; activityMed--;
+        break;
+      case 'e':
+        if (activityMed == 0) break;
+        activityLow++; activityMed--;
+        break;
+      case 'r':
+        if (activityLow == 0) break;
+        activityMed++; activityLow--;
+        break;
+    }
+  }
 }
 
 void printDebugInfo() {
@@ -96,17 +98,17 @@ void loop() {
   lastTime = timeNow;
 
   if(timerOn >= 0) {
-    //ledBrightness = map(timerOn, 0, TIME_ON, 0, 255);
-    ledBrightness = timerOn >= TIME_ON / 2 ? 255 : map(timerOn, 0, TIME_ON / 2, 0, 255);
+    ledBrightness = (timerOn >= TIME_ON / 3) ?
+        MAX_LED_BRIGHTNESS : map(timerOn, 0, TIME_ON / 3, 16, MAX_LED_BRIGHTNESS);
     FastLED.setBrightness(ledBrightness);
     setAllLeds(activityHigh, activityMed, activityLow);
 
-    timerOn -= (int) dt;
+    //timerOn -= (int) dt;
   
   } else {
     setLedsOff();
   }
 
-  printDebugInfo();
+  //printDebugInfo();
   delay(50);
 }
